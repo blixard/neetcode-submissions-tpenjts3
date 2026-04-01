14732d82ead851ee0001e4ca395ea82511ba09ef
Shubhaprasad Padhy <rupelpadhy@gmail.com>
2026-04-01 22:54:03 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    class Key{
+        public int [] freq;
+        public Key(int [] freq){
+            this.freq = freq;
+        }
+
+        public boolean equals(Object o){
+            return Arrays.equals( freq, ((Key)o).freq ); 
+        }
+
+        public int hashCode(){
+            return Arrays.hashCode(freq); 
+        }
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<Key, List<String>> map = new HashMap<>(); 
+        for(String s : strs){
+            int [] count = new int[26]; 
+            for(char ch : s.toCharArray()){
+                count[ch-'a']++; 
+            }
+            Key k = new Key(count); 
+            map.putIfAbsent(k, new ArrayList<String>()); 
+            map.get(k).add(s);
+        }
+        return new ArrayList<>(map.values()); 
+    }
+}
